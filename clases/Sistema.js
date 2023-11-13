d0ed34a3a38626e773c2550f7382ef5259954446
JessicaS161995 <ojitos16@outlook.es>
2023-11-13 10:55:15 -0500

    se intento poner sonido pero no salio

--- clases/Sistema.js
+++ clases/Sistema.js
@@ -1,33 +1,72 @@
+let s;
+let v;
+let sound;
+
+function preload() {
+  // Carga tu archivo de sonido aquí (asegúrate de que el archivo esté en la misma carpeta que tu código)
+  sound = loadSound('0.mp3');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(10, 10, 0);
+  s = new Sistema();
+  v = new Sistema();
+}
+
+function draw() {
+  background(255, 20);
+  s.update();
+  s.display();
+  v.update();
+  v.display();
+}
+
 class Sistema {
-	constructor() {
-		this.pos = createVector(width / 2, height / 2);
-		this.ps = [];
-		this.t = random(100);
-		this.color = color(random(100, 255), random(255), 0);
-	}
-	update() {
-		// 0. Aumentar el tiempo
-		this.t += 0.01;
+  constructor() {
+    this.pos = createVector(width / 2, height / 2);
+    this.ps = [];
+    this.t = random(100);
+    this.color = color(random(100, 255), random(255), 0);
+  }
 
-		// 1. Actualización de posición
-		this.pos.x = map(noise(this.t), 0, 1, 0, width);
-		this.pos.y = map(noise(this.t + 10), 0, 1, 0, height);
+  update() {
+    this.t += 0.01;
 
-		// 2. Agragamos partículas
-		this.p = new Particula(this.pos.x, this.pos.y, this.color);
-		this.ps.push(this.p);
+    this.pos.x = map(noise(this.t), 0, 1, 0, width);
+    this.pos.y = map(noise(this.t + 10), 0, 1, 0, height);
 
-		// 3. Verificar cuáles están muertas y llamar a los macrófagos
-		for (let i = 0; i < this.ps.length; i++) {
-			if (!this.ps[i].isAlive) {
-				this.ps.splice(i, 1);
-			}
-		}
-	}
-	display() {
-		for (let i = 0; i < this.ps.length; i++) {
-			this.ps[i].update();
-			this.ps[i].display();
-		}
-	}
+    // Reproducir sonido al agregar una partícula
+    sound.play();
+
+    this.p = new Particula(this.pos.x, this.pos.y, this.color);
+    this.ps.push(this.p);
+
+    for (let i = 0; i < this.ps.length; i++) {
+      if (!this.ps[i].isAlive) {
+        this.ps.splice(i, 1);
+      }
+    }
+  }
+
+  display() {
+    for (let i = 0; i < this.ps.length; i++) {
+      this.ps[i].update();
+      this.ps[i].display();
+    }
+  }
+}
+
+class Particula {
+  constructor(x, y, color) {
+    // Definir la clase Particula (asegúrate de que está correctamente definida)
+  }
+
+  update() {
+    // Lógica de actualización de partícula
+  }
+
+  display() {
+    // Lógica de visualización de partícula
+  }
 }
